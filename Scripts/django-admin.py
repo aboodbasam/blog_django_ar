6da6aca25187180602a1277f71dfc73529d872f8
Abood Basam <aboodbasam9@outlook.com>
2019-09-08 19:02:11 +0300

    New project and tow apps created

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aboodb~1\desktop\abood\program\python\django\blogger\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
